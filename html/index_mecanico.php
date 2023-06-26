04f877a06131c3662a26a264c63fa01ec024fd13
Alejandro Alvarenga <alejandro.josealvarenga@gmail.com>
2023-06-25 19:55:24 -0600

    Pag del mecanico

---
+++ html/index_mecanico.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mecanico</title>
+    <link rel="stylesheet" href="../css/mec.css">
+    <link rel="icon" href="../src/icon_auto.png" type="image/x-icon">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <ul class="menu">
+                <li><a href="../html/admin.php" class="active">Index</a></li>
+                <li><a href="#">Reserva</a></li>
+                <li><a href="#">Reporte</a></li>
+                <li><a href="#">Vehículo</a></li>
+                <li><a href="#">Reserva</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main wrapper">
+        <div class="content">
+            <h1>Bienvenido Mecanico</h1>
+        </div>
+        <img src="https://img.freepik.com/foto-gratis/mecanico-automoviles-cambiando-ruedas-coche_1303-27465.jpg?w=2000" alt="Imagen de taller" loading="lazy">
+    </main>
+</body>
+
+</html>
